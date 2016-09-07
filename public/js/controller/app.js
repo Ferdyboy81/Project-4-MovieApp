2b387edf5b85ddc631708a929d8b41f2e43903cb
Fernand Tan <ferdyboy81@gmail.com>
2016-09-07 09:45:01 -0700

    set up function for form data in app.js

--- public/js/controller/app.js
+++ public/js/controller/app.js
@@ -30,7 +30,27 @@ movieStubApp.controller("movieDetailsController", function ($scope, $routeParams
 // ticket booking controller
 movieStubApp.controller("bookTicketsController", function ($scope, $http, $location, $routeParams) {
     $scope.getMovieById(routeParams.id);
-}
+    $scope.onlyNumbers = /^\d+$/;
+    $scope.formData = {};
+    $scope.formData.movie_id = $scope.currMovie.id;
+    $scope.formData.movie_name = $scope.currMovie.name;
+    $scope.formData.date = "Today"
+
+    $scope.processForm = function () {
+        $http({
+            method: 'POST',
+            url: '/book',
+            data: $.param($scope.formData), // pass in data as strings
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            } // set the headers so angular passing info as form data (not request payload)
+        })
+            .success(function (data) {
+                //$location.path("/bookings");
+                console.log(data);
+            });
+    };
+});
 
 
 
