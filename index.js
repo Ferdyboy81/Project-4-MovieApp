ac4843d762e6b608c2a3ad62cd8d46e8abed88c4
Fernand Tan <ferdyboy81@gmail.com>
2016-09-06 17:30:12 -0700

    set up angular.resource & service.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+var express = require("express"),
 bodyParser = require('body-parser'),
 morgan = require('morgan');
 
@@ -18,6 +18,11 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', function (req, res) {
     res.render('public/index.html');
 });
+
+app.get('/movies', function (req, res) {
+    var movies = require('./data/movies/json');
+    res.json(movies);
+})
 
 app.listen(port);
 console.log('Express server running at http://localhost:' + port);
